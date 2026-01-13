e7ba37aefe12e9888cc1a2627ac858721b466964
X-lanni <weijun.xie@elephantrobotics.com>
2026-01-13 10:42:13 +0800

    fix(myagv_plus_controller): update launch file

--- myagv_plus_controller/launch/controller.launch.py
+++ myagv_plus_controller/launch/controller.launch.py
@@ -69,7 +69,8 @@ def generate_launch_description():
         remappings=[
             ("~/robot_description", "/robot_description"),
             ("mecanum_drive_controller/reference_unstamped", "/cmd_vel"),
-            ("mecanum_drive_controller/odom", "/odom"),   
+            ("mecanum_drive_controller/odometry", "/odom"),
+            ("mecanum_drive_controller/tf_odometry", "/tf"),
         ],
         condition=UnlessCondition(use_sim),
     )
