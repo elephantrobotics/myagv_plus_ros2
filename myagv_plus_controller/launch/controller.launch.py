97a4819421756df62687fe3932b8cae5df404a72
X-lanni <weijun.xie@elephantrobotics.com>
2026-06-03 15:18:41 +0800

    fix(myagv_plus_controller): increase robot_state_publisher publish frequency

--- myagv_plus_controller/launch/controller.launch.py
+++ myagv_plus_controller/launch/controller.launch.py
@@ -89,7 +89,8 @@ def generate_launch_description():
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="both",
-        parameters=[robot_description],
+        parameters=[robot_description,
+                    {"publish_frequency": 200.0}],
     )
 
     joint_state_broadcaster_spawner = Node(
